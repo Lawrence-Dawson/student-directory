c9dd5acbffe250e726a43102dbd2d37969f03c5e
Lawrence <lawrencedawson@hotmail.co.uk>
2016-08-06 16:13:38 +0100

    Beautiful output

--- directory.rb
+++ directory.rb
@@ -1,30 +1,31 @@
 #lets put all the student in an array
 students = [
- "Dr. Hannibal Lecter",
- "Darth Vader",
- "Nurse Ratched",
- "Michael Corleone",
- "Alex DeLarge",
- "The Wicked Witch of the West",
- "Terminator",
- "Freddy Krueger",
- "The Joker",
- "Joeffrey Baratheon",
- "Norman Bates"]
+ ["Dr. Hannibal Lecter", :november],
+ ["Darth Vader", :november],
+ ["Nurse Ratched", :november],
+ ["Michael Corleone", :november],
+ ["Alex DeLarge", :november],
+ ["The Wicked Witch of the West", :november],
+ ["Terminator", :november],
+ ["Freddy Krueger", :november],
+ ["The Joker", :november],
+ ["Joeffrey Baratheon", :november],
+ ["Norman Bates", :november]
+]
 # and then prints them
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[0]} (#{student[1]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 #nothing happens until we call the methods
 print_header
