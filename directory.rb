ace7333e125209ad07f714675f3e95b1b7dc6e40
Lawrence <lawrencedawson@hotmail.co.uk>
2016-08-07 11:21:11 +0100

    Completed task 4. Added more information to names, eg 'Hobbies'

--- directory.rb
+++ directory.rb
@@ -1,17 +1,30 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the student details".center(10)
+  puts "To finish just leave all options blank"
   # create an empty array
   students = []
-  # get the first name
   name = gets.chomp
+  puts "Age?".center(10)
+  age = gets.chomp
+  puts "Nationality?"
+  nationality = gets.chomp
+  puts "Hobbies?"
+  hobbies = gets.chomp
   # while the name is not empty, repeat this code
-  while !name.empty? do
+   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, age: age, nationality: nationality,
+       hobbies: hobbies, cohort: :november}
     puts "Now we have #{students.count} students"
     #get another name from the user
+    puts "Name?"
     name = gets.chomp
+    puts "Age?"
+    age = gets.chomp
+    puts "Nationality?"
+    nationality = gets.chomp
+    puts "Hobbies?"
+    hobbies = gets.chomp
   end
   #return the array of students
   students
@@ -28,7 +41,7 @@ def print(students)
     students.each.with_index(1) do |student, index|
       if student[:name].length < 12
       student[:name].capitalize!
-      puts "#{index},#{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{index},#{student[:name]} Age:#{student[:age]} Nationality:#{student[:nationality]} Hobbies:#{student[:hobbies]} (#{student[:cohort]} cohort)"
       student_count += 1
     end
   end
