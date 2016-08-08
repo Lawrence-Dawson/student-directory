724d7b52fd6bc88b832adba4914cbcebc0622b88
Lawrence <lawrencedawson@hotmail.co.uk>
2016-08-08 10:30:18 +0100

    Completed task 8. You can now print by the chosen cohort

--- directory.rb
+++ directory.rb
@@ -2,13 +2,13 @@ def input_students
   puts "".center(80,'*')
   puts " Please enter the new students details ".center(80, '*')
   puts
-  puts " To exit program just leave 'name' blank ".center(80, '*')
+  puts " To exit program just leave 'name' blank and type 'Yes' when prompted ".center(80, '*')
   # create an empty array
   students = []
    while true do
     #add the student hash to the array
     puts "Name?"
-    name = gets.chomp
+    name = gets.chomp.capitalize
     if name.empty?
       puts "Do you wish to exit? Yes/No"
       answer = gets.chomp.capitalize
@@ -21,25 +21,25 @@ def input_students
     end
     puts "Age?"
     age = gets.chomp
-
+    #create an array of ages
     ages = [*'1'..'150']
-
+    #check that the age given is a valid age
     until ages.include? age
     puts "Enter a year. E.g. 25"
     age = gets.chomp
     end
-
+    #convert valid age to an integer
     age = age.to_i
 
     puts "Nationality?"
-    nationality = gets.chomp
+    nationality = gets.chomp.capitalize
     puts "Hobbies?"
     hobbies = gets.chomp
     puts "Enter Cohort"
-    cohort = gets.chomp.capitalize!
-
+    cohort = gets.chomp.capitalize
+    #create an array of months
     months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-
+    #check that the month given is a valid month
     until months.include? cohort
       puts "Enter a valid month. E.g. January"
            cohort = gets.chomp.capitalize
@@ -59,21 +59,28 @@ def print_header
 end
 
 def print(students)
-  student_count = 0
-  until student_count == students.length
-    students.each.with_index(1) do |student, index|
-      if student[:name].length < 12
-      student[:name].capitalize!
-      puts "#{index},#{student[:name]}  Age: #{student[:age]}  Nationality: #{student[:nationality]}  Hobbies: #{student[:hobbies]}  (#{student[:cohort]} cohort)"
-      student_count += 1
+  #ask which cohort you wish to find
+  puts "Enter cohort to print"
+  selected = gets.chomp.capitalize
+  #checks that is a valid cohort
+  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  until months.include? selected
+  puts "Enter a valid month. E.g. January"
+  selected = gets.chomp.capitalize
+  end
+  #find selected cohort
+  to_print = students.select {|student| student[:cohort] == selected }
+  #prints each student within that cohort
+  to_print.each do |student|
+    puts "".center(80,":")
+    puts "#{student[:name]}  Age: #{student[:age]}  Nationality: #{student[:nationality]}  Hobbies: #{student[:hobbies]}  (#{student[:cohort]} cohort)"
     end
   end
-end
 
 def print_footer(students)
   puts " Overall, we have #{students.count} great students ".center(80,'*')
 end
-end
+
 
 students = input_students
 #nothing happens until we call the methods
