8d66690897fd68a3c0471187bd1ed72b0ed3b44f
Lawrence <lawrencedawson@hotmail.co.uk>
2016-08-08 13:55:22 +0100

    Interactive Menu is working.

--- directory.rb
+++ directory.rb
@@ -1,3 +1,28 @@
+def interactive_menu
+  students = []
+  loop do
+  #1. print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+  #2. read the input and save it into a variable
+  selection = gets.chomp
+  #3. do what the user has asked
+  case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit #this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
   puts "".center(80,'*')
   puts " Please enter the new students details ".center(80, '*')
@@ -90,9 +115,4 @@ def print_footer(students)
   puts " Overall, we have #{students.count} great students ".center(80,'*')
 end
 
-
-students = input_students
-#nothing happens until we call the methods
-print_header
-print(students)
-print_footer(students)
+interactive_menu
